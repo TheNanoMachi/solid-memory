2c4a58f3508a39b397fe5e9687c407f3a9c8a86e
TheNanoMachi <anthonyl99087@gmail.com>
2021-05-05 15:17:30 -0400

    added steel

--- src/main/java/thenanomachi/weapons/and/tools/expanded/steel.java
+++ src/main/java/thenanomachi/weapons/and/tools/expanded/steel.java
@@ -10,37 +10,31 @@ public class steel implements ToolMaterial{
 
     @Override
     public float getAttackDamage() {
-        // TODO Auto-generated method stub
         return 6.5F;
     }
 
     @Override
     public int getDurability() {
-        // TODO Auto-generated method stub
         return 750;
     }
 
     @Override
     public int getEnchantability() {
-        // TODO Auto-generated method stub
         return 10;
     }
 
     @Override
     public int getMiningLevel() {
-        // TODO Auto-generated method stub
         return 3;
     }
 
     @Override
     public float getMiningSpeedMultiplier() {
-        // TODO Auto-generated method stub
         return 6.0F;
     }
 
     @Override
     public Ingredient getRepairIngredient() {
-        // TODO Auto-generated method stub
         return Ingredient.ofItems(Items.IRON_INGOT);
     }
 
